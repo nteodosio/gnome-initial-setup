b2d5f229f3537c28802213770b2d51a0e1211830
Nathan Pratta Teodosio <nathan.teodosio@canonical.com>
2022-08-11 08:12:30 -0300

    Make apply function return false on last page

--- gnome-initial-setup/pages/ubuntu-pro/gis-ubuntupro-page.c
+++ gnome-initial-setup/pages/ubuntu-pro/gis-ubuntupro-page.c
@@ -672,20 +672,26 @@ gis_ubuntupro_page_apply (GisPage      *gis_page,
 {
   GisUbuntuProPage *page = GIS_UBUNTUPRO_PAGE (gis_page);
   GisUbuntuProPagePrivate *priv = gis_ubuntupro_page_get_instance_private (page);
-  static counter = 2;
-  if (counter == 2){
+  static int counter = 1;
+  counter++;
+  g_print("Counter: %d\n", counter);
+  if (counter == 2) {
     on_magic_toggled(NULL, GIS_UBUNTUPRO_PAGE2(priv->page2));
     gis_page_set_complete (GIS_PAGE (page), FALSE);
     gis_page_apply_complete (GIS_PAGE (page), FALSE);
     gtk_stack_set_visible_child (GTK_STACK (priv->stack), priv->page2);
-    counter++;
+    return TRUE;
   } else if (counter == 3) {
     GisUbuntuProPage3 *page3 = GIS_UBUNTUPRO_PAGE3(priv->page3);
     display_ua_services(gis_ubuntupro_page3_get_instance_private (page3));
     gtk_stack_set_visible_child (GTK_STACK (priv->stack), priv->page3);
     gis_assistant_next_page (gis_driver_get_assistant (GIS_PAGE (page)->driver));
+    return TRUE;
+  } else {
+    gis_page_set_complete (GIS_PAGE (page), TRUE);
+    gis_page_apply_complete (GIS_PAGE (page), TRUE);
+    return FALSE;
   }
-  return TRUE;
 }
 
 static void
